c18f98c095f99ae5b7e13063c120ede1b889d58e
dmcvicar <mlops.solutions@gmail.com>
2024-10-11 11:26:37 -0600

    resume as pdf

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,5 +2,12 @@ import InfoTab from "@/components/info_tab";
 import resume_content from "../../../public/content/resume.json";
 
 export default function Page() {
-  return <InfoTab {...resume_content} />;
+  return (
+    <>
+      <InfoTab {...resume_content} />
+      <div className="flex mt-8 justify-center text-lg hover:underline">
+        <a href="/David McVicar - Resume 09_24.pdf">Resume as PDF</a>
+      </div>
+    </>
+  );
 }
